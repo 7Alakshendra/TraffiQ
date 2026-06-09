34c2cddf4c7fe1c73595cb5628d81143af6a92ca
7Alakshendra <alakshendravasisht@gmail.com>
2026-06-09 13:57:54 +0530

    initial data pre-processing

--- model/train.py
+++ model/train.py
@@ -2,9 +2,42 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.model_selection import train_test_split 
 
 BASE_DIR = os.path.dirname(__file__)
 data_path = os.path.join(BASE_DIR, '..', 'data', 'raw', 'Banglore_traffic_Dataset.csv')
 
 df = pd.read_csv(data_path)
-print(df.shape)
+df["Date"]=pd.to_datetime(df["Date"])
+df['DayofWeek'] = df['Date'].dt.dayofweek
+df['Month'] = df['Date'].dt.month
+print(df.info())
+
+features = [
+    'Area Name',
+    'Traffic Volume',
+    'Average Speed',
+    'Travel Time Index',
+    'Weather Conditions',
+    'Roadwork and Construction Activity',
+    'DayofWeek',
+    'Month'
+]
+
+target = 'Congestion Level'
+
+X = df[features]
+y = df[target]
+
+print(X.shape)
+print(y.shape)
+print(X.info())
+
+cat_cols=["Area Name","Weather Conditions","Roadwork and Construction Activity"]
+
+X=pd.get_dummies(X,columns=cat_cols,drop_first=True)
+
+print(X.shape)
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+print(X_train.shape,X_test.shape)
